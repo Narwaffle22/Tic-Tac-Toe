70869ba60d72da103f6cb2e5816246582cc4fb83
Narwaffle22 <nate222v@icloud.com>
2024-09-16 16:41:01 -0500

    some basic stuffs

--- tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
@@ -2,19 +2,47 @@
 //
 
 #include <iostream>
+#include <cassert>
+
+using namespace std;
+
+class Board {
+public:
+    Board();
+    string getBoard();
+
+private:
+    string currentBoard;
+    string slots[9] = { "1","2","3","4","5","6","7","8","9" };
+    string startingBoard = "   |   |   \n-- - +-- - +-- - \n   |   |   \n-- - +-- - +-- - \n   |   |   ";
+};
+
+Board::Board() {
+    this->currentBoard = startingBoard;
+}
+Board::getBoard() {
+    return currentBoard;
+}
+
+class Boardprinter {
+public:
+    Boardprinter();
+
+private:
+    string something;
+    string edgeMarker = " ";
+    string verticalDivider = " | ";
+    string horizontalDivider = "\n---+---+---\n";
+};
+
+
+
+Boardprinter::Boardprinter() {
+    this->something = " ";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Board* board = new Board();
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
