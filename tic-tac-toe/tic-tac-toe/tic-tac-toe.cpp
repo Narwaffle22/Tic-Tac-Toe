5da6173e5e2ccf38f9f4aa04d38f99e6ea594cce
Narwaffle22 <nate222v@icloud.com>
2024-09-17 16:50:27 -0500

    mark and pprint boad

--- tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
@@ -6,43 +6,66 @@
 
 using namespace std;
 
+
+#pragma region Board Class
+
 class Board {
 public:
     Board();
     string getBoard();
+    void markBoard(int, string);
+    string checkTile(int);
+    void printBoard();
 
 private:
     string currentBoard;
     string slots[9] = { "1","2","3","4","5","6","7","8","9" };
-    string startingBoard = "   |   |   \n-- - +-- - +-- - \n   |   |   \n-- - +-- - +-- - \n   |   |   ";
+    string startingBoard = " 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 ";
+    string edgeMarker = " ";
+    string verticalDivider = " | ";
+    string horizontalDivider = "\n---+---+---\n";
+    void formatBoard();
 };
 
 Board::Board() {
     this->currentBoard = startingBoard;
 }
-Board::getBoard() {
+string Board::getBoard() {
     return currentBoard;
 }
-
-class Boardprinter {
-public:
-    Boardprinter();
-
-private:
-    string something;
-    string edgeMarker = " ";
-    string verticalDivider = " | ";
-    string horizontalDivider = "\n---+---+---\n";
-};
-
-
-
-Boardprinter::Boardprinter() {
-    this->something = " ";
+void Board::markBoard(int slot, string XorO) {
+    slots[slot - 1] = XorO;
+    formatBoard();
+}
+string Board::checkTile(int slot) {
+    return "";
+}
+void Board::formatBoard() {
+    currentBoard = edgeMarker + slots[0] + verticalDivider + slots[1] + verticalDivider + slots[2] + edgeMarker;
+    currentBoard += horizontalDivider;
+    currentBoard += edgeMarker + slots[3] + verticalDivider + slots[4] + verticalDivider + slots[5] + edgeMarker;
+    currentBoard += horizontalDivider;
+    currentBoard += edgeMarker + slots[6] + verticalDivider + slots[7] + verticalDivider + slots[8] + edgeMarker;
+}
+void Board::printBoard() {
+    cout << "\n" << currentBoard;
 }
 
+#pragma endregion
+
+// main 
 int main()
 {
     Board* board = new Board();
+
+    board->printBoard();
+
+    board->markBoard(3, "X");
+
+    board->printBoard();
+
+    board->markBoard(7, "O");
+
+    board->printBoard();
 
 }
